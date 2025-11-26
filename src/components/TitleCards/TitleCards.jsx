0e129569341124a9aff9840c2e10073ad989c253
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-11-26 11:53:52 +0100

    feat: Added the sign up and sign in page Created an account on TMDB to get the api credentials which were added to the env for fetching list of movies which is listed on the homepage Added react-router-dom for handling navigations

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,49 @@
 import './TitleCards.css'
+import { useEffect, useRef, useState } from 'react'
 
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+  const cardsRef = useRef();
+  const [apiData, setApiData] = useState([]);
+
+  const readAccessToken = import.meta.env.MOVIE_DB_API_ACCESS_TOKEN;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer '+readAccessToken
+    }
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:'now_playing'}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, [])
+
   return (
-    <div className="titlecards">TitleCards</div>
+    <div className="title-cards">
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt={card.original_title} />
+              <p>{card.original_title}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 export default TitleCards
