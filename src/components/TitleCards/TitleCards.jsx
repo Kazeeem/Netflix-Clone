96e3e0616af23788401d5df76965d8f197006b8b
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-11-26 12:35:37 +0100

    feat: Worked on the video player page Added dark background to the navbar when the page get scrolled down

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,13 @@
-import './TitleCards.css'
+import { Link } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react'
+import './TitleCards.css'
 
 
 const TitleCards = ({title, category}) => {
   const cardsRef = useRef();
   const [apiData, setApiData] = useState([]);
 
-  const readAccessToken = import.meta.env.MOVIE_DB_API_ACCESS_TOKEN;
+  const readAccessToken = import.meta.env.VITE_MOVIE_DB_API_ACCESS_TOKEN;
 
   const options = {
     method: 'GET',
@@ -36,10 +37,10 @@ const TitleCards = ({title, category}) => {
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
               <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt={card.original_title} />
               <p>{card.original_title}</p>
-            </div>
+            </Link>
           )
         })}
       </div>
